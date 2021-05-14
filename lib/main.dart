9848b11b805c2a27a23ff8ff3f9733cabcd762c8
MartinaVin <martina.vincoli@studenti.unitn.it>
2021-05-14 21:04:43 +0200

    primula

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:kaleidokids/presentation/app.dart';
+import 'package:kaleidokids/presentation/pages/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
