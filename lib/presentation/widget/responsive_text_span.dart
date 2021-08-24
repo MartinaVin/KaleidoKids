0b8fdde2775c17134e42f5716dbc61ffe54c59c2
Martina Vincoli <martina.vincoli@studenti.unitn.it>
2021-08-24 11:36:50 +0200

    Added random colors to login buttons

---
+++ lib/presentation/widget/responsive_text_span.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:kaleidokids/compute_size.dart';
+
+class ResponsiveText extends StatelessWidget {
+  const ResponsiveText(this.text, {Key key, this.style, this.textAlign})
+      : super(key: key);
+
+  final String text;
+  final TextStyle style;
+  final TextAlign textAlign;
+
+  @override
+  Widget build(BuildContext context) {
+    final double textSize = computeSize(context);
+    return TextSpan(
+      text,
+      style:
+          style?.copyWith(fontSize: textSize) ?? TextStyle(fontSize: textSize),
+    );
+  }
+}
